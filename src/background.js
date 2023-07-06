446c11f0e8ce54dd10a9f0ebfef85a3bc38edf80
Yauhen Shulitski <jsnjack@gmail.com>
2023-07-07 00:20:54 +0200

    :p: Fix setting proxified path

--- src/background.js
+++ src/background.js
@@ -25,9 +25,16 @@ function encodeDomain(domain) {
     return domain.replace(/0/g, '00').replace(/-/g, '-0-').replace(/\./g, '-');
 }
 
-function toDomainPath(url) {
-    const { hostname } = new URL(url);
-    let parts = hostname.split('.');
+function toDomainPath(data) {
+    if (data.includes('://')) {
+        data = new URL(data).hostname;
+    } else {
+        while (data[0] === '.') {
+            // according to RFC, '.example.com' and 'example.com' is the same thing
+            data = data.slice(1);
+        }
+    }
+    let parts = data.split('.');
     return '/' + parts.reverse().join('/');
 }
 
@@ -69,7 +76,7 @@ function navigationHandler(details) {
                     expirationDate: cookie.expirationDate,
                     httpOnly: cookie.httpOnly,
                     name: encode_cookie_key(cookie.name, cookie.path),
-                    path: toDomainPath(details.url),
+                    path: toDomainPath(cookie.domain),
                     secure: cookie.secure,
                     value: encode_cookie_value(cookie.value, cookie.secure),
                 };
